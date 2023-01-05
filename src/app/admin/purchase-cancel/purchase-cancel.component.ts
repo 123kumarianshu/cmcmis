aa54d9b1ab909f982c3ce5abc674e6a550ffc442
KunwarSahilPratepChandel <sahilkumar000000123@gmail.com>
2023-01-05 15:56:14 +0530

    responsive sale cancel sale cancle purchase

--- src/app/admin/purchase-cancel/purchase-cancel.component.ts
+++ src/app/admin/purchase-cancel/purchase-cancel.component.ts
@@ -10,7 +10,7 @@ import { ManageService } from '../manage.service';
   styleUrls: ['./purchase-cancel.component.css']
 })
 export class PurchaseCancelComponent implements OnInit {
-  displayedColumns: string[] = ['slno','cust_shop_name', 'Item_name', 'cust_owner_name', 'cust_contact_no','Action', 'Gst','Date', 'Gross amount', 'Cancel_reasion','View_notice'];
+  displayedColumns: string[] = ['slno','cust_shop_name', 'Item_name', 'cust_owner_name', 'cust_contact_no', 'Cancel_reasion','Date',];
   dataSource!: MatTableDataSource<any>;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
